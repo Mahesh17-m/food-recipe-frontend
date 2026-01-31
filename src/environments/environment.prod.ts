b70bb978d1049d91be261f38822a4e851f4093c0
Mahesh17-m <mbmandalapu200217@gmail.com>
2026-01-31 16:18:52 +0530

    add oauth

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,11 @@ export const environment = {
   production: true,                    // ← true for production
   apiUrl: 'https://food-recipe-backend-yqx9.onrender.com/api', // ← live backend
   imageBaseUrl: 'https://food-recipe-backend-yqx9.onrender.com',
+   googleClientId: '911074336198-07hqjv87j2glss1b5a6i7tna4nr439tc.apps.googleusercontent.com',
+  // Add your Vercel domain here
+  frontendUrl: 'https://food-recipe-frontend-rust.vercel.app',
+  // Also include for auth redirects
+  redirectUri: 'https://food-recipe-frontend-rust.vercel.app',
   endpoints: {
     recipes: '/recipes',
     auth: '/auth',
